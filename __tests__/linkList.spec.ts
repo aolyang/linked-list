145b1a2bfd7e1fcf7b4debb3e6aff1c1e193b382
aolyang <aolyang@foxmail.com>
2022-06-30 02:36:14 +0800

    fix(api): fix forEach, test forEach and insert

--- __tests__/linkList.spec.ts
+++ __tests__/linkList.spec.ts
@@ -39,9 +39,88 @@ describe("test linkedList", () => {
       expect(list.head).toBeTruthy()
       expect(list.tail).toBeTruthy()
       expect(list.head!.data).toEqual(_d[0])
+      expect(list.head!.next!.data).toEqual(_d[1])
       expect(list.tail!.data).toEqual(_d[_d.length - 1])
+      expect(list.tail!.prev!.data).toEqual(_d[_d.length - 2])
     })
   })
+
   // api
-  //
+  it("forEach order correctly", () => {
+    const list = new LinkedList(data)
+    list.forEach((node,index) => {
+      expect(node.data).toEqual(data[index])
+    })
+  })
+
+
+  it("node insert correctly", () => {
+    let list = new LinkedList()
+    list.insert(1)
+
+    // [ 1 ]
+    expect(list.size).toEqual(1)
+    expect(list.head).toEqual(list.tail)
+    expect(list.head!.next).toEqual(list.tail!.prev)
+    expect(list.head!.next).toEqual(null)
+    expect(list.head!.data).toEqual(list.tail!.data)
+    expect(list.head!.data).toEqual(1)
+
+    // [ 1, 2 ]
+    list.insert(2)
+    expect(list.size).toEqual(2)
+    expect(list.head!.data).toEqual(1)
+    expect(list.head!.next!.data).toEqual(2)
+    expect(list.tail!.data).toEqual(2)
+    expect(list.tail!.prev!.data).toEqual(1)
+
+    // [ 1, 2, 3 ]
+    list.insert(3)
+    expect(list.size).toEqual(3)
+    expect(list.tail!.data).toEqual(3)
+    expect(list.tail!.prev).toEqual(list.head!.next)
+    expect(list.head!.next!.data).toEqual(list.tail!.prev!.data)
+
+    // [ 0, 1, 2, 3 ]
+    list.insert(0, 0)
+    expect(list.size).toEqual(4)
+    expect(list.head!.data).toEqual(0)
+    expect(list.head!.next!.data).toEqual(1)
+    expect(list.tail!.data).toEqual(3)
+    expect(list.tail!.prev!.data).toEqual(2)
+
+    /*
+    *                   head  1️⃣, 2, 3, 4
+    * [ 0, 1, 2, 3 ]  => [ 0, -1, 1, 2, 3 ]
+    * */
+    list.insert(-1, 1)
+    expect(list.size).toEqual(5)
+    expect(list.head!.data).toEqual(0)
+    expect(list.head!.next!.data).toEqual(-1)
+
+    /*
+    *  0,  1, 2, 3, 4      head  1  2  3  4️⃣  5
+    *[ 0, -1, 1, 2, 3 ] => [ 0, -1, 1, 2, -3, 3 ]
+    * */
+    list.insert(-3, list.size - 1)
+    expect(list.size).toEqual(6)
+    expect(list.head!.data).toEqual(0)
+    expect(list.tail!.data).toEqual(3)
+    expect(list.head!.next!.data).toEqual(-1)
+    expect(list.tail!.prev!.data).toEqual(-3)
+
+
+    /*
+    *
+    * head  1  2  3   4  5        head  1  2️⃣   3  4   5, 6
+    * [ 0, -1, 1, 2, -3, 3 ]  =>  [ 0, -1, -4, 1, 2, -3, 3 ]
+    * */
+    list.insert(-4, 2)
+    expect(list.size).toEqual(7)
+    expect(list.tail!.data).toEqual(3)
+    expect(list.tail!.prev!.data).toEqual(-3)
+    expect(list.head!.data).toEqual(0)
+    expect(list.head!.next!.data).toEqual(-1)
+    expect(list.head!.next!.next!.data).toEqual(-4)
+  })
 })
--- lib/linkedList.ts
+++ lib/linkedList.ts
@@ -46,18 +46,53 @@ class LinkedList<T> {
   insert(data: T, index?: number) {
     const node = new LinkedListNode(data)
 
-    if (index === undefined) {
-      if (this.tail) {
-        this.tail.next = node
-        node.prev = this.tail
+    let ok = false
+    if (this.size === 0) {
+      this.head = this.tail = node
+      ok = true
+    } else if (index === undefined) {
+      if (this.size === 1) {
+        this.head!.next = node
+        node.prev = this.head
         this.tail = node
       } else {
+        this.tail!.next = node
+        node.prev = this.tail
         this.tail = node
       }
+      ok = true
     } else {
+      this.forEach((_node, _index, stop) => {
+        if (_index === index) {
+          if (index === 0) {
+            node.next = this.head
+            this.head = node
+          } else if (index === 1) {
+            const next = this.head!.next!
+            next.prev = node
+            node.next = next
+            node.prev = this.head
+            this.head!.next = node
+          } else if (index === (this.size - 1)) {
+            node.prev = this.tail!.prev
+            node.next = this.tail
+            node.prev!.next = node
+            this.tail!.prev = node
+          } else {
+            node.prev = _node.prev
+            node.prev!.next = node
 
+            node.next = _node
+            _node.prev = node
+          }
+
+          stop()
+          ok = true
+        }
+      })
     }
-
+    if (ok) this.size++
+    return ok
   }
 
   forEach<R = T>(fn: (node: LinkedListNode<R>, index: number, stop: () => void) => any) {
@@ -68,10 +103,10 @@ class LinkedList<T> {
     const stop = () => {
       _break = true
     }
-    while (cur) {
+    while (cur && this.size > index) {
       fn(cur, index, stop)
       if (_break) break
-      cur = this.head.next
+      cur = cur.next
       index++
     }
   }
@@ -104,6 +139,17 @@ class LinkedList<T> {
       }
     })
     return _index
+  }
+
+  find<R = T>(fn: (node: LinkedListNode<R>, index: number) => boolean): LinkedListNode<R> | undefined {
+    let _node: LinkedListNode<R> | undefined = undefined
+    this.forEach<R>((node, index, stop) => {
+      if (fn(node, index)) {
+        _node = node
+        stop()
+      }
+    })
+    return _node
   }
 
   push(...data: T[]) {
